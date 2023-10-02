7e0970e290def92ddcf1353e78e87fba8a17e3db
VHU5129 <robert.koncz@urss.hu>
2023-10-02 17:21:37 +0200

    add base files

---
+++ js/field.js
@@ -0,0 +1,34 @@
+export default class Field {
+  #eventOnField; // property event, card event, jail event
+  #fieldName;
+  #value;
+  #owner = null;
+  #domElement;
+  #color;
+  #nrOfHouses;
+  #basePenalty;
+
+  constructor(name, value, eventOnField) {
+
+  }
+
+  get fieldName() {
+    return this.#fieldName;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  get owner() {
+    return this.#owner;
+  }
+
+  set owner(newOwner) {
+    this.#owner = newOwner;
+  }
+
+
+
+
+}
---
+++ js/game.js
@@ -0,0 +1,22 @@
+class Game {
+ #diceRolled;
+
+
+}
+
+class Board {
+  #fields;
+  #players;
+
+
+  #generateBoard () {
+
+  }
+
+}
+
+
+
+class Player {}
+
+
