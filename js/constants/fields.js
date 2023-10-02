e4a600f10d4f831c69c68da5e3e6912641fc24b3
VHU5129 <robert.koncz@urss.hu>
2023-10-02 18:35:00 +0200

    add fields constant

--- js/constants/fields.js
+++ js/constants/fields.js
@@ -1,3 +1,185 @@
 export const fields = [
-
-]
+  {
+    name: "GO",
+    value: 0,
+  },
+  {
+    name: "VINE STREET",
+    value: 15,
+  },
+  {
+    name: "Community chest",
+    value: 0,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Coventry street",
+    value: 57,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Income tax",
+    value: 0,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Marylebone station",
+    value: 500,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Leicester square",
+    value: 68,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Chance",
+    value: 0,
+    eventFn: (targetPlayer) => {},
+  },
+  {
+    name: "Bow street",
+    value: 81,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Jail",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "The angel islington",
+    value: 91,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Electric company",
+    value: 1240,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Trafalgar square",
+    value: 97,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Northumrld avenue",
+    value: 112,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Fenchurch st. Station",
+    value: 700,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Mborough Street",
+    value: 125,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Community chest",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Fleet street",
+    value: 148,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Old kent road",
+    value: 208,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Parking lot",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Whitehall",
+    value: 211,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Chance",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Pentonville Road",
+    value: 215,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Pall Mall",
+    value: 228,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "King Cross station",
+    value: 1000,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Bond street",
+    value: 271,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Strand",
+    value: 320,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Go to Jail",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Euston road",
+    value: 404,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Piccadilly",
+    value: 440,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Community chest",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Oxford street",
+    value: 550,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Liverpool station",
+    value: 1500,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Chance",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Park Lane",
+    value: 562,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Super tax",
+    value: 0,
+    event: (targetPlayer) => {},
+  },
+  {
+    name: "Mayfair",
+    value: 1800,
+    event: (targetPlayer) => {},
+  },
+];
