e64b3af8ddd46993755421cccfbb4671d076592b
VHU5129 <robert.koncz@urss.hu>
2023-10-03 18:04:09 +0200

    rename diceControls

--- js/controls.js
+++ js/controls.js
@@ -1,6 +1,4 @@
-import diceRoll from "./util/diceRoll.js";
-
-export default class Controls {
+export default class DiceControls {
   #domElement;
 
   constructor() {}
@@ -8,20 +6,21 @@ export default class Controls {
   initControls() {
     const buttonContainer = document.createElement("DIV");
     const rollButton = document.createElement("BUTTON");
-    rollButton.textContent = 'Roll the dice!'
+    rollButton.textContent = "Roll the dice!";
+    rollButton.id = "roll-btn"
     buttonContainer.append(rollButton);
 
     buttonContainer.style.position = "absolute";
-    buttonContainer.style.right = "0px";
-    buttonContainer.style.top = "0px";
+    buttonContainer.style.right = "5px";
+    buttonContainer.style.top = "5px";
 
     buttonContainer.height = "10em";
     buttonContainer.width = "10em";
     buttonContainer.border = "1px solid black";
 
-    rollButton.addEventListener('click',() => {
-      return diceRoll();
-    })
-    document.body.append(buttonContainer);
+    this.#domElement = buttonContainer;
+  }
+  get domElement() {
+    return this.#domElement;
   }
 }
