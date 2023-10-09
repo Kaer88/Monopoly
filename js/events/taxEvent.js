b58e3b27ec7916f4231e1abf82e631ac0c9a2c98
VHU5129 <robert.koncz@urss.hu>
2023-10-09 16:12:33 +0200

    add tax event fn

--- js/events/taxEvent.js
+++ js/events/taxEvent.js
@@ -1,3 +1,23 @@
-export default function () {
+/**
+ *
+ * @param player {Player}
+ * @param fieldInfo {Field}
+ * @param allFields {Field[]}
+ * @param refreshScoreFn {function}
+ */
+import ScoreBoard from "../ScoreBoard";
 
+export default function taxEvent(player, fieldInfo, allFields, refreshScoreFn) {
+  return new Promise((resolve) => {
+    const payBtn = document.querySelector("#pay-btn");
+    ScoreBoard.instance.newMessage("Tax must be paid!");
+    const payFn = () => {
+      player.balance -= fieldInfo.value;
+      payBtn.disabled = true;
+      payBtn.removeEventListener("click", payFn);
+      resolve();
+    };
+    payBtn.disabled = false;
+    payBtn.addEventListener("click", payFn);
+  });
 }
