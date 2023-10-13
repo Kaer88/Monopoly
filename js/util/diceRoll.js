ca4a184fa7dec33941e694399d3c840a07c4f3f0
VHU5129 <robert.koncz@urss.hu>
2023-10-13 09:27:45 +0200

    change diceroll to help testing

--- js/util/diceRoll.js
+++ js/util/diceRoll.js
@@ -1,3 +1,7 @@
 export default function diceRoll() {
-  return [Math.floor(Math.random() * 6 + 1), Math.floor(Math.random() * 6 + 1)];
+  const dice1 = Number(prompt("egyes dobás:"));
+  const dice2 = Number(prompt("kettes dobás:"));
+  return [dice1, dice2];
+
+  //return [Math.floor(Math.random() * 6 + 1), Math.floor(Math.random() * 6 + 1)];
 }
