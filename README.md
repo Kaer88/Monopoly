8c13e2b8bc3b71ec0ba0bf3967ac5364085626f5
VHU5129 <robert.koncz@urss.hu>
2023-10-27 08:35:58 +0200

    move todos to readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Monopoly
+
+## TODOs:
+
+- card events, buttons as card
+- build component for modal
+- parent modal element could be a singleton => remove html child when closed
+    - add some border radius, proper close button 
+- utility events rent calc (?)
+- name input, color picker and start game button
+- sell property dialog component
+- make it look nice
+- BUG: can go below 0 with paying prison fine and still roll
--- js/Game.js
+++ js/Game.js
@@ -1,22 +1,6 @@
 import Board from "./Board.js";
 import ScoreBoard from "./ScoreBoard.js";
 
-
-/*
-TODO:
-
-
-- card events, buttons as card
-- build component for modal
-- parent modal element could be a singleton => remove html child when closed
-    - add some border radius, proper close button 
-- utility events rent calc (?)
-- name input, color picker and start game button
-- sell property dialog component
-- make it look nice
-- BUG: can go below 0 with paying prison fine and still roll
-
- */
 class Game {
   #board;
   #gameState;
